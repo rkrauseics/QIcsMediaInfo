257cc58ad42a9e2a2e0333288bf2a51d6dc8fc17
bquinn <bquinn@ics.com>
2016-03-07 10:33:10 -0500

    output now goes to JSON

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #if ! defined QT_NO_DEBUG_OUTPUT
 //#define QT_NO_DEBUG_OUTPUT
 #endif
+#include <QTextStream>
 #include <QDebug>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QDir>
+#include <QRegularExpression>
 
 #include <QCoreApplication>
 #include <QCommandLineParser>
@@ -15,6 +20,9 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    int nfiles  = 0;
+
+    QMap<QString, QStringList> parametersMap;
     QStringList generalParams;
     generalParams << "CompleteName" << "FolderName" << "FileName" << "FileExtension"
                   << "FileSize" << "Duration" << "FrameRate" << "BitRate" << "BitRate_Mode/String"
@@ -22,12 +30,12 @@ int main(int argc, char *argv[])
                   << "Title" << "Season" << "Movie"
                   << "Album" << "Album_More" << "Album/Performer"
                   << "Part" << "Track" << "Track/Position" << "Compilation"
-                  << "Performer" << "Genre" << "Mood\\n%"
+                  << "Performer" << "Genre" << "Mood"
                   << "Released_Date" << "Original/Released_Date" << "Recorded_Date"
                   << "Encoded_Application" << "Encoded_Library" << "BPM"
                   << "Cover" << "Cover_Mime" << "Lyrics"
                   << "Added_Date";
-
+    parametersMap.insert("General",generalParams);
     QString generalInform;
     generalInform="General;";
     foreach(QString s, generalParams) {
@@ -39,6 +47,7 @@ int main(int argc, char *argv[])
     videoParams << "Format"<<"BitRate" << "BitRate_Mode/String"
                 << "Width" << "Height" << "DisplayAspectRatio/String" << "Rotation/String"
                 << "FrameRate" << "BitDepth" << "Title" << "Language/String";
+    parametersMap.insert("Video",videoParams);
     QString videoInform;
     videoInform="Video;";
     foreach(QString s, videoParams) {
@@ -48,8 +57,9 @@ int main(int argc, char *argv[])
 
     QStringList audioParams;
     audioParams << "Format" << "BitRate" << "BitRate_Mode/String"
-                  << "Channel(s)" << "SamplingRate" << "StreamSize"
-                  << "Title" << " Language/String";
+                << "Channel(s)" << "SamplingRate" << "StreamSize"
+                << "Title" << " Language/String";
+    parametersMap.insert("Audio",audioParams);
     QString audioInform;
     audioInform="Audio;";
     foreach(QString s, audioParams) {
@@ -58,20 +68,16 @@ int main(int argc, char *argv[])
     audioInform+="\\n";
 
     MediaInfoList MI;
-
+    QMap<QString, QString> infoMap;
     MI.Option(__T("ParseSpeed"), __T("0"));
     MI.Option(__T("Language"), __T("raw"));
     MI.Option(__T("ReadByHuman"), __T("0"));
     MI.Option(__T("Legacy"), __T("0"));
 
-    QString infoVersion=QString::fromStdWString(MI.Option(__T("Info_Version"), __T("0.7.82;MediaInfo_Example;0.7.82")).c_str());
-    qDebug() << Q_FUNC_INFO << "Info_Version" << infoVersion;
+//    QString infoVersion=QString::fromStdWString(MI.Option(__T("Info_Version"), __T("0.7.82;MediaInfo_Example;0.7.82")).c_str());
+//    qDebug() << Q_FUNC_INFO << "Info_Version" << infoVersion;
 
     // TODO:
-    // Commandline options for
-    // -p --params
-    // -c --codecs
-    // -g general -s sound -v video -a all
     // output is json
     // argument is a directory
     QCommandLineParser cmdLine;
@@ -88,21 +94,31 @@ int main(int argc, char *argv[])
     QCommandLineOption allOptions("a");
     cmdLine.addOption(allOptions);
     cmdLine.process(a);
-    //
-// if --params
+
+    // if --params
     if(cmdLine.isSet(parametersOptions)){
         QString infoParameters=QString::fromStdWString( MI.Option(__T("Info_Parameters")));
         qDebug() << Q_FUNC_INFO << "\r\n\r\nInfo_Parameters\r\n" << qPrintable(infoParameters);
     }
-// if --codecs
+    // if --codecs
     if(cmdLine.isSet(codecsOptions)){
         QString infoCodecs=QString::fromStdWString( MI.Option(__T("Info_Codecs")).c_str() );
         qDebug() << Q_FUNC_INFO << qPrintable("\r\n\r\nInfo_Codecs\r\n") << qPrintable(infoCodecs);
     }
 
-    qDebug() << Q_FUNC_INFO << qPrintable("\r\n\r\nOpen\r\n");
-    const int nfiles=MI.Open(__T("/Users/bquinn/Downloads/mp3s"));
-    qDebug() << Q_FUNC_INFO << "Opened" << nfiles << "files";
+//    qDebug() << Q_FUNC_INFO << qPrintable("\r\n\r\nOpen\r\n");
+    QString dirPath ="";
+    if(cmdLine.positionalArguments().count() == 1)
+        dirPath = cmdLine.positionalArguments()[0];
+
+    QDir inDir(dirPath);
+    if(!inDir.exists()){
+        MI.Close();
+        qFatal( dirPath.toLocal8Bit() + "does not exist");
+    }
+    else{
+        nfiles = MI.Open(dirPath.toStdWString(),MediaInfoLib::FileOption_NoRecursive);
+        qDebug() << Q_FUNC_INFO << "Opened" << nfiles << "files";
 
 //        qDebug() << Q_FUNC_INFO << qPrintable("\r\n\r\nInform with Complete=false\r\n");
 //        MI.Option(__T("Complete"));
@@ -114,56 +130,63 @@ int main(int argc, char *argv[])
 //        QString completeInform=QString::fromStdWString(MI.Inform().c_str());
 //        qDebug() << Q_FUNC_INFO << qPrintable(completeInform);
 
-    // if -a || -g
-    if(cmdLine.isSet(generalOptions) || cmdLine.isSet(allOptions)){
+        // if -a || -g
+        if(cmdLine.isSet(generalOptions) || cmdLine.isSet(allOptions)){
+            MI.Option(QStringLiteral("Inform").toStdWString(), generalInform.toStdWString());
+            QString informOptionExample=QString::fromStdWString(MI.Inform());
+            infoMap.insert("General", informOptionExample);
+//            qDebug() << qPrintable("\r\n\r\nGeneral Inform\r\n") << qPrintable(informOptionExample);
+        }
 
-        MI.Option(QStringLiteral("Inform").toStdWString(), generalInform.toStdWString());
-        QString informOptionExample=QString::fromStdWString(MI.Inform());
-        qDebug() << qPrintable("\r\n\r\nGeneral Inform\r\n") << qPrintable(informOptionExample);
-}
+        // if -a || -v
+        if(cmdLine.isSet(videoOptions) || cmdLine.isSet(allOptions)){
+            MI.Option(QStringLiteral("Inform").toStdWString(), videoInform.toStdWString());
+            QString videoOptionExample=QString::fromStdWString(MI.Inform());
+            infoMap.insert("Video", videoOptionExample);
+//            qDebug() << qPrintable("\r\n\r\nVideo Inform\r\n") << qPrintable(videoOptionExample);
+        }
 
-    // if -a || -v
-    if(cmdLine.isSet(videoOptions) || cmdLine.isSet(allOptions)){
-        MI.Option(QStringLiteral("Inform").toStdWString(), videoInform.toStdWString());
-        QString videoOptionExample=QString::fromStdWString(MI.Inform());
-        qDebug() << qPrintable("\r\n\r\nVideo Inform\r\n") << qPrintable(videoOptionExample);
-}
+        // if -a || -s
+        if(cmdLine.isSet(soundOptions) || cmdLine.isSet(allOptions)){
+            MI.Option(QStringLiteral("Inform").toStdWString(), audioInform.toStdWString());
+            QString audioOptionExample=QString::fromStdWString(MI.Inform());
+            infoMap.insert("Audio", audioOptionExample);
+//            qDebug() << qPrintable("\r\n\r\nAudio Inform\r\n") << qPrintable(audioOptionExample);
+        }
 
-    // if -a || -s
-    if(cmdLine.isSet(soundOptions) || cmdLine.isSet(allOptions)){
-        MI.Option(QStringLiteral("Inform").toStdWString(), audioInform.toStdWString());
-        QString audioOptionExample=QString::fromStdWString(MI.Inform());
-        qDebug() << qPrintable("\r\n\r\nAudio Inform\r\n") << qPrintable(audioOptionExample);
-}
+        QJsonDocument outDoc;
+        foreach(QString section, infoMap.keys())
+        {
+            QStringList informResult = infoMap[section].split(QRegularExpression("\r?\n"));
+            if(informResult.last().isEmpty())
+                informResult.pop_back();
+            QVariantMap resMap;
+           qDebug() << qPrintable(section) <<" Information";
+            foreach (QString res, informResult) {
+                QStringList resList = res.split(QRegularExpression(":(?!\\\\)"));
+                if(resResult.last().isEmpty())
+                    resResult.pop_back();
+                Q_ASSERT(resList.count() == generalParams.count());
+                for (int i = 0;i < parametersMap[section].count(); i++) {
+                        resMap[parametersMap[section][i]] = resList[i];
+                }
+                QJsonObject resObject=QJsonObject::fromVariantMap(resMap);
+                QString mimeType=resMap["InternetMediaType"].toString().toLower();
+                if(!mimeType.contains("audio") && !mimeType.contains("video")){
+                    qWarning() << Q_FUNC_INFO << "mimetype for file" << resMap["CompleteName"]<< "not one of audio or video but" << resMap["InternetMediaType"].toString();
+                }
+                else{
+                    outDoc.setObject(resObject);
+                    qDebug()<< qPrintable(outDoc.toJson());
+                }
+            }
+        }
+    }
 
-//
 //        QString generalFileSize=QString::fromStdWString(MI.Get(1,Stream_General, 0, __T("FileSize"), Info_Text, Info_Name));
 //        qDebug() << Q_FUNC_INFO << "General: FileSize:" << generalFileSize;
 
-        MI.Close();
-        QTimer::singleShot(nfiles * 100, &a, SLOT(quit()));
-       return a.exec();
+    MI.Close();
+    QTimer::singleShot(nfiles * 100, &a, SLOT(quit()));
+    return a.exec();
 }
-
-
-//nfiles=MI.Open(dirPath.toStdWString(),MediaInfoLib::FileOption_NoRecursive);
-//QStringList informResult=informOptionExample.split('\n',QString::SkipEmptyParts);
-//QVariantMap resMap;
-//foreach (QString res, informResult) {
-//    qDebug() << Q_FUNC_INFO << res;
-//    QStringList resList=res.split(":");
-////        qDebug() << resList.count() << generalParams.count();
-//    Q_ASSERT((resList.count()-1)==generalParams.count());
-//    for (int i=0;i<resList.count()-1;++i) {
-//        qDebug() << generalParams[i] << ":" << resList[i];
-//        resMap[generalParams[i]] = resList[i];
-//    }
-//    QJsonObject resObject=QJsonObject::fromVariantMap(resMap);
-//    QString mimeType=resMap["InternetMediaType"].toString();
-//    if (mimeType.startsWith("audio")) m_audioFiles.append(resObject);
-//    else if (mimeType.startsWith("video")) m_videoFiles.append(resObject);
-//    else {
-//        qWarning() << Q_FUNC_INFO << "mimetype for file" << resMap["CompleteName"]<< "not one of audio or video but" << resMap["InternetMediaType"];
-//    }
-//    qDebug() << Q_FUNC_INFO << "resObject" << resObject;
-//}
